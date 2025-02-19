9a31264e4710e4aa3f029a50d62125fbf7440b74
IgorRaiad0 <igorsonic56@gmail.com>
2025-02-18 23:21:40 -0300

    Sistema previamente funcionando e estilizado

--- routes/ADM.js
+++ routes/ADM.js
@@ -5,7 +5,7 @@ import MedicoController from "../controllers/medicoController.js";
 import admController from "../controllers/admController.js";
 
 
-// Rotas Pacientes
+// Rotas PacientesAdm
 router.get('/index', admController.index);
 router.get('/cadastroPaciente', PacienteController.cadastro);
 router.post('/salvarPaciente', PacienteController.salvar);
@@ -14,7 +14,7 @@ router.get('/editarPaciente/:id', PacienteController.editarPaciente);
 router.post('/atualizarPaciente/:id', PacienteController.atualizarPaciente);
 router.post('/apagarPaciente/:id', PacienteController.apagarPaciente);
 
-// Rotas Médicos
+// Rotas MédicosADM
 router.get('/cadastroMedico', MedicoController.cadastroMedico);
 router.post('/salvarMedico', MedicoController.salvarMedico);
 router.get('/listagemMedicos', MedicoController.listagemMedico);
